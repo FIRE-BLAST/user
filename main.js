229778d98cd7b02dd1c921193c3b0ac724d45702
FIRE-BLAST <ankitprakash074@gmail.com>
2023-10-07 21:14:27 +0530

    local

--- main.js
+++ main.js
@@ -1,20 +1,30 @@
-window.onload = function() {
+//window.onload = function() {
 
     // Check for LocalStorage support.
-    if (localStorage) {
+    //if (localStorage) {
   
       // Add an event listener for form submissions
-      document.getElementById('my-form').addEventListener('submit', function() {
+      //document.getElementById('my-form').addEventListener('submit', function() {
         // Get the value of the name field.
-        var name = document.getElementById('name').value;
-        var email = document.getElementById('email').value;
+        //var name = document.getElementById('name').value;
+        //var email = document.getElementById('email').value;
   
         // Save the name in localStorage.
-        localStorage.setItem('name', name);
-        localStorage.setItem('Email', email);
-      });
+        //localStorage.setItem('name', name);
+        //localStorage.setItem('Email', email);
+     // });
   
-    }
+    //}
   
-  }
+  //}
+
+
+let myObj = {
+    name: "axz",
+    age: 22
+};
+let myObj_serialised = JSON.stringify(myObj);
+localStorage.setItem("myObj",myObj_serialised);
+let myObj_deserialised = JSON.parse(localStorage.getItem("myObj"));
+
 
