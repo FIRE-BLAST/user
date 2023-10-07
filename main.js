5093fe97f51a6cce07be339dcfa255cf7e205f2c
FIRE-BLAST <ankitprakash074@gmail.com>
2023-10-07 20:56:49 +0530

    form

---
+++ main.js
@@ -0,0 +1,20 @@
+window.onload = function() {
+
+    // Check for LocalStorage support.
+    if (localStorage) {
+  
+      // Add an event listener for form submissions
+      document.getElementById('my-form').addEventListener('submit', function() {
+        // Get the value of the name field.
+        var name = document.getElementById('name').value;
+        var email = document.getElementById('email').value;
+  
+        // Save the name in localStorage.
+        localStorage.setItem('name', name);
+        localStorage.setItem('Email', email);
+      });
+  
+    }
+  
+  }
+
